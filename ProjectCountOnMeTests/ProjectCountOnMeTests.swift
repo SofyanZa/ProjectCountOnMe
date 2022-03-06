bdb4f41e5f271826cbdb092316cf0ced3d2b2abb
SofyanZa <soza@live.fr>
2022-03-07 00:19:43 +0100

    add test file

--- ProjectCountOnMeTests/ProjectCountOnMeTests.swift
+++ ProjectCountOnMeTests/ProjectCountOnMeTests.swift
@@ -1,36 +1,93 @@
 //
 //  ProjectCountOnMeTests.swift
-//  ProjectCountOnMeTests
 //
 //  Created by SofyanZ on 06/03/2022.
-//
 
 import XCTest
+
 @testable import ProjectCountOnMe
 
-class ProjectCountOnMeTests: XCTestCase {
+class CalculatorTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var calculator: Calculator!
+
+    //Methode setUp permet de faire une initialisation
+    override func setUp() {
+        super.setUp()
+        calculator = Calculator()
+    }
+    
+    
+    func testGivenCanAddOperator_WhenCanAddOperator_ThenReturnTrue() {
+        calculator.calculString = " 2 + 2 + 3 "
+        XCTAssertTrue(calculator.canAddOperator)
+    }
+    
+    
+    func testGivenExpressionHaveResult_WhenExpressionHaveResult_ThenReturnTrue() {
+          calculator.calculString = " = 30 "
+          XCTAssertTrue(calculator.canAddOperator)
+    }
+    
+    func testGivenAddNumber_WhenAddNumber_ThenReturnNumber() {
+          calculator.addNumbers(numbers: "3")
+          XCTAssertTrue(calculator.calculString == "3")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    
+    //Checking expression is correct: 2 + 2 = 4
+    func testGivenExpressionIsCorrect_WhenStringGreaterThanOrEqualTo3_ThenReturnTrue() {
+        calculator.addNumbers(numbers: "2")
+        calculator.addOperator(with: "+")
+        calculator.addNumbers(numbers: "2")
+        calculator.addEqual()
+        XCTAssertEqual(calculator.calculString, "2 + 2 = 4")
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testGivenNumberOne_WhenExpressionDontHaveEnoughElement_ThenAlertMessage() {
+        calculator.addNumbers(numbers: "5")
+        calculator.addEqual()
+        XCTAssertEqual(calculator.calculString, "5")
     }
+    
+    func testGivenPresentationCalcul_WhenTryMultiplicationOrAddition_ThenShowAlert() {
+        calculator.addAC()
+        calculator.addOperator(with: "x")
+        calculator.addOperator(with: "+")
+        
+        //Error: Vous ne pouvez pas commencez par un opérateur !
+        XCTAssertEqual(calculator.calculString, "")
+    }
+    
+    func testGivenNumberOne_WhenTryDivideByZero_ThenInitialiseNil() {
+        calculator.addNumbers(numbers: "1")
+        calculator.addOperator(with: "÷")
+        calculator.addNumbers(numbers: "0")
+        calculator.addEqual()
+        
+        //Error: Impossible de diviser par 0
+        XCTAssertEqual(calculator.calculString, "")
+    }
+    
+    func testGivenNumberOne_WhenDoFormat_ThenGiveResult() {
+        calculator.addNumbers(numbers: "10")
+        calculator.addOperator(with: "÷")
+        calculator.addNumbers(numbers: "3")
+        calculator.addEqual()
+        
+        XCTAssertEqual(calculator.calculString, "10 ÷ 3 = 3.333")
+    }
+    
+    func testGivenNumberOne_WhenApplyTenZero_ThenResultCorect() {
+        calculator.addNumbers(numbers: "10000000000")
+        calculator.addOperator(with: "x")
+        calculator.addNumbers(numbers: "10000000000")
+        calculator.addEqual()
 
+        XCTAssertEqual(calculator.calculString, "10000000000 x 10000000000 = 1e+20")
+    }
+    
+        
 }
+    
