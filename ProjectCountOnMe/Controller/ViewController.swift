798247aa68ff411e23b3ac5696e4f41eca240834
SofyanZa <soza@live.fr>
2022-03-13 14:01:41 +0100

    add protocols & factorization

--- ProjectCountOnMe/Controller/ViewController.swift
+++ ProjectCountOnMe/Controller/ViewController.swift
@@ -8,75 +8,48 @@
 
 import UIKit
 
-
-
 class ViewController: UIViewController {
     
-
     // MARK: PROPERTIES
-    
-    let calculator = Calculator()
-    
+    private let calculator = Calculator()
     
     // MARK: - OUTLETS connect the View and the Controller
-    
-    
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
     
-    
     // MARK: - ACTIONS connect between the View and The methods of Controller
-    
-    
     @IBAction func tappedNumberButton(_ sender: UIButton) {
-        guard let numberText = sender.title(for: .normal) else {
-            return
-        }
+        guard let numberText = sender.title(for: .normal) else { return }
         calculator.addNumbers(numbers: numberText)
     }
     
-
     @IBAction func tappetheOperator(_ sender: UIButton) {
         guard let mathOperator = sender.titleLabel?.text else { return }
         calculator.addOperator(with: mathOperator)
     }
-
     
     @IBAction func tappedEqualButton(_ sender: UIButton) {
         calculator.addEqual()
     }
     
-    
     @IBAction func tappedACButton(_ sender: UIButton) {
         calculator.addAC()
-        
     }
-    
     
     // MARK: - VIEW LIFE CYCLES
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        bind()
+        calculator.delegate = self
     }
-    
-    
-    // MARK: - METHODS
-    
-    
-    func alertMessage(message: String) -> Void {
+}
+    extension ViewController: CalculatorProtocol {
+    func presentAlert(with message: String) {
         let alertVC = UIAlertController(title: "Oups", message: message, preferredStyle: .alert)
         alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-        self.present(alertVC, animated: true, completion: nil)
-        
+        self.present(alertVC,animated: true, completion: nil)
     }
     
-
-    func bind(){
-        calculator.alertMessage = alertMessage
-        calculator.calculText = { calculText in
-            self.textView.text = calculText
-        }
+    func presentResult(with calculationString: String) {
+        textView.text = calculationString
     }
-
 }
--- ProjectCountOnMe/Model/Calculator.swift
+++ ProjectCountOnMe/Model/Calculator.swift
@@ -8,74 +8,59 @@
 
 import Foundation
 
+protocol CalculatorProtocol: AnyObject {
+    func presentAlert(with message : String)
+    func presentResult(with calculationString: String)
+}
 
 final class Calculator {
+    // MARK: Outputs
     
+    weak var delegate: CalculatorProtocol?
     
-    // MARK:  Outputs
-    
-    var alertMessage: ((String) -> Void)?
-    
-    var calculText: ((String) -> Void)?
-    
-    // MARK: - Initialization
-    
+    // MARK: Initialization
     init() {
         self.calculString = ""
     }
-
+    
     // MARK: - Properties
-    
-    
     // Cette variable permet d'afficher dans le textView de la View
     var calculString: String {
         didSet {
-            calculText?(calculString)
+            delegate?.presentResult(with: calculString)
+            // calculText?(calculString)
         }
     }
-    
     // var elements pour creer les nombres
     private var elements: [String] {
         return calculString.split(separator: " ").map { "\($0)" }
     }
-    
     // Ces variables permettent de verifier le calcul
     var expressionIsCorrect: Bool {
         return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "÷"
     }
-    
     var expressionHaveEnoughElement: Bool {
         return elements.count >= 3
     }
-    
     var canAddOperator: Bool {
         return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "÷"
     }
-    
     var expressionHaveResult: Bool {
         return calculString.firstIndex(of: "=") != nil
     }
-    
     // var isDivideByZero est false
     var isDivideByZero: Bool {
         return calculString.contains("÷ 0")
     }
-    
     // Permet de commencer avec un nombre
-    var isPossibleToStartWithNumber : Bool {
+    var isPossibleToStartWithNumber: Bool {
         if calculString >= "0" && calculString <= "9"{
-            return elements.count >= 1 }
-        else {
-            alertMessage?("Vous ne pouvez pas commencer par un opérateur !")
-        }
+            return elements.count >= 1 } else {
+                delegate?.presentAlert(with: "Vous ne pouvez pas commencer par un opérateur")
+            }
         return false
     }
-    
-    
-    
-    //MARK:- METHODS
-    
-    
+    // MARK: - METHODS
     // func addNumbers qui vérifie si l'on peut ajouter un nombre
     func addNumbers(numbers: String) {
         if expressionHaveResult {
@@ -83,120 +68,85 @@ final class Calculator {
         }
         calculString.append(numbers)
     }
-    
     // func addOperator qui vérifie si l'on peut ajouter un opérateur
     func addOperator(with mathOperator: String) {
         if isPossibleToStartWithNumber {
             if canAddOperator {
-                 calculString.append(" \(mathOperator) ")
-             } else {
-                 alertMessage?("Un operateur est déja mis !")
-             }
+                calculString.append(" \(mathOperator) ")
+            } else {
+                delegate?.presentAlert(with: "Un opérateur est deja mis")
+            }
         }
     }
     
-    // func addAC permet de remettre à 0
     func addAC() {
         calculString.removeAll()
-        calculText?("0")
     }
     
-    
-    // func addEqual qui effectue le calcul final avec l'expression, si tous les conditions sont remplies
     func addEqual() {
-        
-        // guard data existe dans tout le scope de la fonction
         guard expressionIsCorrect else {
-            alertMessage?("Entrez une expression correcte !")
+            delegate?.presentAlert(with: "Entrez une expression correct")
             return
         }
-        
         guard expressionHaveEnoughElement else {
-            alertMessage?("Démarrez un nouveau calcul !")
+            delegate?.presentAlert(with: "Démarrez un nouveau calcul")
             return
         }
-        
         guard !isDivideByZero else {
-            alertMessage?("Impossible de diviser par 0 !")
+            delegate?.presentAlert(with: "Impossible de diviser par zero")
             calculString = ""
             return
         }
-        
-        // Création d'une copie locale des opérations
+        // Si tout est vérifié on lance la fonction getResult
+        getResult()
+    }
+    
+    func getResult() {
         var operationsToReduce = elements
-        
         while operationsToReduce.count > 1 {
             
             guard var left = Double(operationsToReduce[0]) else { return }
-           var operand = operationsToReduce[1]
+            var operand = operationsToReduce[1]
             guard var right = Double(operationsToReduce[2]) else { return }
-            
             let result: Double
-                
-                // Commencer à un ou nous ne pouvons pas attribuer d'index à (index - 1)
-                var operandIndex = 1
-                
-                // Recherche s'il y a plusieurs signes de division puis attribuer un index
-                if let index = operationsToReduce.firstIndex(where: { $0 == "x" || $0 == "÷" }) {
-                    
-                    operandIndex = index
-                    if let leftunwrapp = Double(operationsToReduce[index - 1]) { left = leftunwrapp }
-                    operand = operationsToReduce[index]
-                    if let rightUnwrapp = Double(operationsToReduce[index + 1]) { right = rightUnwrapp }
-                }
-                
-                result = calculate(left: Double(left), right: Double(right), operand: operand)
-                
             
-                // Boucle à l'intérieur de l'index pour supprimer l'opérateur supplémentaire
-                for _ in 1...3 {
-                    
-                    operationsToReduce.remove(at: operandIndex - 1)
-                    print("Je supprime :\(operationsToReduce)")
-                }
+            var operandIndex = 1
             
-                operationsToReduce.insert(formatResult(result: Double(result)), at: operandIndex - 1 )
-                print("Resultat :\(result)")
-                print("Le resultat apres formatResult :\(operationsToReduce)")
-                
+            // Priorité des opérations
+            if let index = operationsToReduce.firstIndex(where: { $0 == "x" || $0 == "÷" }) {
+                operandIndex = index
+                if let leftunwrapp = Double(operationsToReduce[index - 1]) { left = leftunwrapp }
+                operand = operationsToReduce[index]
+                if let rightUnwrapp = Double(operationsToReduce[index + 1]) { right = rightUnwrapp }
             }
-            guard let finalResult = operationsToReduce.first else { return }
-            calculString.append(" = \(finalResult)")
-            print("Le resultat de finalResult :\(finalResult)")
+            result = calculate(left: Double(left), right: Double(right), operand: operand)
+            
+            for _ in 1...3 {
+                operationsToReduce.remove(at: operandIndex - 1)
+            }
+            operationsToReduce.insert(formatResult(result: Double(result)), at: operandIndex - 1 )
+        }
+        guard let finalResult = operationsToReduce.first else { return }
+        calculString.append(" = \(finalResult)")
     }
-    
-    
-
-    
-    
-       func calculate(left: Double, right: Double, operand: String) -> Double {
-           
-           let result: Double
-           switch operand {
-           case "+": result = left + right
-           case "-": result = left - right
-           case "÷": result = left / right
-           case "x": result = left * right
-           default: return 0.0
-           }
-           return result
-       }
-    
-    // func formatResult permet de gérer les nombres décimaux
-     private func formatResult(result: Double) -> String {
-          let formatter = NumberFormatter()
-          formatter.maximumFractionDigits = 3
-          guard let resultFormated = formatter.string(from: NSNumber(value: result)) else { return String() }
-          
-        // Sinon on met la puissance pour des grands nombres aux petits nombres
-          guard resultFormated.count <= 10 else {
-              return String(result)
-          }
-          return resultFormated
-      }
-    
 }
-
-
-
-
+    func calculate(left: Double, right: Double, operand: String) -> Double {
+    let result: Double
+    switch operand {
+    case "+": result = left + right
+    case "-": result = left - right
+    case "÷": result = left / right
+    case "x": result = left * right
+    default: return 0.0
+    }
+    return result
+}
+    private func formatResult(result: Double) -> String {
+    let formatter = NumberFormatter()
+    formatter.maximumFractionDigits = 3
+    guard let resultFormated = formatter.string(from: NSNumber(value: result)) else { return String() }
+    // Sinon on met la puissance pour des grands nombres aux petits nombres
+    guard resultFormated.count <= 10 else {
+        return String(result)}
+    return resultFormated
+}
